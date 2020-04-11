77036e42a051ea38a4a14f0cddbe764d74480e71
hackerdmitry <hacker.dmitry@gmail.com>
2020-04-11 18:54:14 +0500

    Правый и левый повороты в КЧ дереве

--- RBTree/Node.cs
+++ RBTree/Node.cs
@@ -5,16 +5,38 @@ namespace RBTree {
     public class Node<T> where T : IComparable<T>
     {
         public Node<T> Parent { get; set; }
-        
+        public Node<T> Uncle
+        {
+            get
+            {
+                var parent = Parent;
+                if (parent == null)
+                {
+                    return null;
+                }
+                
+                parent.CheckChildNodes();
+                return Disposition == DispositionNode.Left
+                           ? parent.Right
+                           : parent.Left;
+            }
+        }
+        public Node<T> GrandParent => Parent?.Parent;
+
         public Node<T> Left { get; set; }
         public Node<T> Right { get; set; }
 
         public NodeColor Color { get; set; }
+        public DispositionNode? Disposition => Parent == null
+                                                   ? (DispositionNode?) null
+                                                   : this > Parent
+                                                       ? DispositionNode.Right
+                                                       : DispositionNode.Left;
 
         public T Value { get; set; }
 
         public bool IsRoot => Parent == null;
-        public bool IsNIL => Left == null && Right == null;
+        public bool IsNil => Left == null && Right == null;
 
         public void CheckChildNodes()
         {
@@ -23,11 +45,50 @@ namespace RBTree {
                 throw new NotNormalChildNodes<T>(this);
             }
         }
+        
+        public bool IsRedParentAndRedUncle()
+        {
+            return !IsRoot && 
+                   Parent.Color == NodeColor.Red && 
+                   Uncle.Color == NodeColor.Red;
+        }
+        
+        public bool IsRedParentAndBlackUncle()
+        {
+            return !IsRoot && 
+                   Parent.Color == NodeColor.Red && 
+                   Uncle.Color == NodeColor.Black;
+        }
+
+        public void ChangeColor()
+        {
+            Color = IsRoot
+                        ? NodeColor.Black
+                        : Color == NodeColor.Red
+                            ? NodeColor.Black
+                            : NodeColor.Red;
+        }
+
+        public static bool operator >(Node<T> node1, Node<T> node2)
+        {
+            return node1.Value.CompareTo(node2.Value) > 0;
+        }
+
+        public static bool operator <(Node<T> node1, Node<T> node2)
+        {
+            return node1.Value.CompareTo(node2.Value) < 0;
+        }
     }
 
     public enum NodeColor
     {
         Red,
         Black
+    }
+
+    public enum DispositionNode
+    {
+        Left,
+        Right
     }
 }
--- RBTree/RBTree.cs
+++ RBTree/RBTree.cs
@@ -14,9 +14,147 @@ namespace RBTree
 
         public RBTree(T initial)
         {
-            Root = new Node<T> {Value = initial};
+            RefreshRootNode(initial);
         }
 
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                RefreshRootNode(value);
+                return;
+            }
+
+            var newNode = CreateNode(value);
+            var curNode = Root;
+            while (!curNode.IsNil)
+            {
+                curNode = newNode > curNode ? curNode.Right : curNode.Left;
+            }
+
+            SetParent(newNode, curNode.Parent);
+            if (newNode.IsRedParentAndRedUncle())
+            {
+                RectifySituationRedParentAndRedUncle(newNode);
+            } 
+            else if (newNode.IsRedParentAndBlackUncle())
+            {
+                RectifySituationRedParentAndBlackUncle(newNode);
+            }
+        }
+
+        private void RectifySituationRedParentAndRedUncle(Node<T> node)
+        {
+            do
+            {
+                var grandParent = node.GrandParent;
+
+                grandParent.ChangeColor();
+                grandParent.Left.ChangeColor();
+                grandParent.Right.ChangeColor();
+
+                node = grandParent;
+            }
+            while (node.IsRedParentAndRedUncle());
+        }
+
+        private void RectifySituationRedParentAndBlackUncle(Node<T> node)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RotateRight(Node<T> node)
+        {
+            var parent = node.Parent;
+
+            var leftChild = node.Left;
+            var leftChildRightGrandson = leftChild.Right;
+
+            node.Left = leftChildRightGrandson;
+            node.Parent = leftChild;
+            leftChild.Right = node;
+            if (parent != null)
+            {
+                SetParent(leftChild, parent);
+            }
+            
+            if (node.Color == NodeColor.Black && 
+                leftChild.Color == NodeColor.Red)
+            {
+                leftChild.ChangeColor();
+                node.ChangeColor();
+            }
+        }
+
+        private void RotateLeft(Node<T> node)
+        {
+            var parent = node.Parent;
+
+            var rightChild = node.Right;
+            var rightChildLeftGrandson = rightChild.Left;
+
+            node.Right = rightChildLeftGrandson;
+            node.Parent = rightChild;
+            rightChild.Left = node;
+            if (parent != null)
+            {
+                SetParent(rightChild, parent);
+            }
+            
+            if (node.Color == NodeColor.Black && 
+                rightChild.Color == NodeColor.Red)
+            {
+                rightChild.ChangeColor();
+                node.ChangeColor();
+            }
+        }
+
+        private static void SetParent(Node<T> node, Node<T> parent)
+        {
+            node.Parent = parent;
+            if (node > parent)
+            {
+                parent.Right = node;
+            }
+            else
+            {
+                parent.Left = node;
+            }
+        }
+
+        private void RefreshRootNode(T value)
+        {
+            var leftNil = CreateNil();
+            var rightNil = CreateNil();
+            Root = new Node<T>
+            {
+                Color = NodeColor.Black,
+                Value = value,
+                Left = leftNil,
+                Right = rightNil
+            };
+        }
+
+        private static Node<T> CreateNode(T value)
+        {
+            var leftNil = CreateNil();
+            var rightNil = CreateNil();
+            return new Node<T>
+            {
+                Color = NodeColor.Red,
+                Value = value,
+                Left = leftNil,
+                Right = rightNil
+            };
+        }
+
+        private static Node<T> CreateNil()
+        {
+            return new Node<T> {Color = NodeColor.Black};
+        }
+
+    #region Проверка свойств КЧ дерева
+        
         /// <summary>
         /// Каждый узел окрашен либо в красный, либо в черный цвет
         /// </summary>
@@ -45,7 +183,7 @@ namespace RBTree
             while (queue.Count > 0)
             {
                 var node = queue.Dequeue();
-                if (node.IsNIL)
+                if (node.IsNil)
                 {
                     if (node.Color == NodeColor.Black)
                     {
@@ -107,7 +245,7 @@ namespace RBTree
             while (queue.Count > 0)
             {
                 var (node, height) = queue.Dequeue();
-                if (node.IsNIL)
+                if (node.IsNil)
                 {
                     if (blackHeight == -1)
                     {
@@ -141,5 +279,7 @@ namespace RBTree
                    IsTrueProperty4() &&
                    IsTrueProperty5();
         }
+
+    #endregion
     }
 }
